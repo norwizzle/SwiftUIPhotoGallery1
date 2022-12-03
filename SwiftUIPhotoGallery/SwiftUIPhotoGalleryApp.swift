0024963391ee693b016cf0360916f37b9c2e3ee2
hutility-norwin <norwin@hutility.com>
2022-12-03 15:49:09 -0800

    Initial Commit

---
+++ SwiftUIPhotoGallery/SwiftUIPhotoGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPhotoGalleryApp.swift
+//  SwiftUIPhotoGallery
+//
+//  Created by Norwin Uy on 2022-12-03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPhotoGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
